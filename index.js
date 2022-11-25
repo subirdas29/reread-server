92733fcdb2ea351463b640b635381c8701dcbf80
subirdas29 <dasjoy104@gmail.com>
2022-11-26 01:24:40 +0600

    role added

--- index.js
+++ index.js
@@ -53,6 +53,21 @@ async function run (){
             res.send({ isAdmin :user?.role ==='admin' })
         })
 
+        app.get('/users/buyer/:email',async(req,res)=>
+        {
+            const email = req.params.email
+            const query = {email}
+            const user = await usersCollection.findOne(query)
+            res.send({ isBuyer :user?.role ==='User' })
+        })
+        app.get('/users/seller/:email',async(req,res)=>
+        {
+            const email = req.params.email
+            const query = {email}
+            const user = await usersCollection.findOne(query)
+            res.send({ isSeller :user?.role ==='Seller' })
+        })
+
     }
     finally{
 
