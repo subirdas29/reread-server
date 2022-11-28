b8376f2eb79342034597d651e59da42e5cd67fcc
subirdas29 <dasjoy104@gmail.com>
2022-11-28 11:41:43 +0600

    my order added

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const { query } = require('express');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1mua1t2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run (){
 
     try{
@@ -22,6 +23,7 @@ async function run (){
         const categoriesCollection = client.db('reRead').collection('categories')
         const booksCollection = client.db('reRead').collection('books')
         const usersCollection = client.db('reRead').collection('users')
+        const myOrdersCollection = client.db('reRead').collection('myOrders')
       
         app.get('/categories',async(req,res)=>{
             const query = {}
@@ -121,19 +123,19 @@ async function run (){
            
         })
 
-        app.put('/allbooks/:id', async(req,res)=>
+        app.patch('/allbooks/:id', async(req,res)=>
         {
            
            const id = req.params.id;
            const filter = ({_id:ObjectId(id)})
-           const options = { upsert: true };
+          
           
            const updateDoc = {
              $set: {
               advertisement:'true'
              },
            };
-           const result = await booksCollection.updateOne(filter, updateDoc, options)
+           const result = await booksCollection.updateOne(filter, updateDoc)
            res.send(result)
         })
 
@@ -150,7 +152,7 @@ async function run (){
             const email = req.params.email;
            const filter = ({email})
            console.log(email)
-        //    const options = { upsert: true };
+   
 
            const updateDoc = {
              $set: {
@@ -165,7 +167,7 @@ async function run (){
         {
             const id = req.params.id;
            const filter = ({_id:ObjectId(id)})
-        //    const options = { upsert: true };
+      
 
            const updateDoc = {
              $set: {
@@ -177,22 +179,21 @@ async function run (){
           
         })
 
-        // app.get('/users/allseller/:email'),async(req,res)=>{
-        //     const status = req.params.email;
-        //     const query=({email})
-        //     // const query = (status:status)
-        //     const result = await usersCollection.find(query).toArray()
-        //    res.send(result)
-
-        // }
-
-        app.get('/users/verifyseller',async(req,res)=>
+        app.post('/myorders',async(req,res)=>
         {
-            const query = {status:"verified"}
-            const user = await usersCollection.find(query).toArray()
-           res.send(user)
-          
+            const order = req.body;
+            const result = await myOrdersCollection.insertOne(order)
+            res.send(result)
         })
+        app.get('/myorders',async(req,res)=>
+        {
+            const query= {}
+            const result = await myOrdersCollection.find(query).toArray()
+            res.send(result)
+        })
+       
+
+        
     }
 
     finally{
